de2d3dff85688f5a81bc0d77d504c465781509a6
saipavwan <40223002+saipavwan@users.noreply.github.com>
2019-01-29 15:30:36 -0800

    Update stack.sh

--- stack.sh
+++ stack.sh
@@ -4,7 +4,7 @@
 							
 							sudo yum -y install wget
 							wget  http://www-us.apache.org/dist/tomcat/tomcat-9/v9.0.14/bin/apache-tomcat-9.0.14.tar.gz
-							sudo tar -zxvf apache-tomcat-8.0.33.tar.gz -C /opt/tomcat --strip-components=1
+							sudo tar -zxvf apache-tomcat-9.0.14.tar.gz -C /opt/tomcat --strip-components=1
 						         cd /usr/share/tomcat/bin
 							./shutdown.sh
 							./startup.sh
