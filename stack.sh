f8026019839f2da0ce6b5309d5ea1c10955c07af
saipavwan <40223002+saipavwan@users.noreply.github.com>
2019-02-08 16:40:52 -0800

    Update stack.sh

--- stack.sh
+++ stack.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 							
-							
+							 cat /var/spool/mail/root
+
 							
 							sudo yum -y install wget
 							wget  http://www-us.apache.org/dist/tomcat/tomcat-9/v9.0.14/bin/apache-tomcat-9.0.14.tar.gz
