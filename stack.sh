7a4de5b6823f3fadab216f245c5ddfab11ba2aae
saipavwan <40223002+saipavwan@users.noreply.github.com>
2019-02-19 14:14:47 -0800

    Update stack.sh

--- stack.sh
+++ stack.sh
@@ -1,15 +1,14 @@
 #!/bin/bash                                                         
-                                                                      chmod 777 /var/spool/mail/root
-                                                                     cat /var/spool/mail/root
-							
-							sudo yum -y install wget
-							wget  http://www-us.apache.org/dist/tomcat/tomcat-9/v9.0.14/bin/apache-tomcat-9.0.14.tar.gz
-							sudo tar -zxvf apache-tomcat-9.0.14.tar.gz -C /opt/tomcat --strip-components=1
-						         cd /usr/share/tomcat/bin
-							./shutdown.sh
-							./startup.sh
-							cd /usr/share/tomcat/webapps
-							cd /usr/share/tomcat/bin
-							./shutdown.sh
-							./startup.sh
-							{
+chmod 777 /var/spool/mail/root
+cat /var/spool/mail/root
+sudo yum -y install wget
+wget  http://www-us.apache.org/dist/tomcat/tomcat-9/v9.0.14/bin/apache-tomcat-9.0.14.tar.gz
+sudo tar -zxvf apache-tomcat-9.0.14.tar.gz -C /opt/tomcat --strip-components=1
+cd /tomcat/bin
+./shutdown.sh
+./startup.sh
+cd /usr/share/tomcat/webapps
+cd /usr/share/tomcat/bin
+./shutdown.sh
+./startup.sh
+
