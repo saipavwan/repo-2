93bb1251aa05042facb6b68a0747ae9840444b5f
saipavwan <40223002+saipavwan@users.noreply.github.com>
2019-03-01 13:54:29 -0800

    Update stack.sh

--- stack.sh
+++ stack.sh
@@ -2,11 +2,11 @@ echo "------------------------executing script------------------------------"
 echo
 echo "------------------------executing jenkins build------------------------------"
 umask 0002
-timeout 20s curl -INs http://build.jenkins.columbiadevops.com/ | head -n 1 >> /home/jenkins/dashboard/workspace/site_Build_jenkins_test
+timeout 20s curl -INs http://build.jenkins.columbiadevops.com/ | head -n 1 >> site_Build_jenkins_test
+chmod 777 site_Build_jenkins_test
+timeout 20s curl -INs http://jenkins.colmdev.com/ | head -n 1 >>  site_X1_jenkins_test
 
-timeout 20s curl -INs http://jenkins.colmdev.com/ | head -n 1 >>  /home/jenkins/dashboard/workspace/site_X1_jenkins_test
-
-timeout 20s curl -NIs http://test.jenkins.columbiadevops.com/ | head -n 1 >> /home/jenkins/dashboard/workspace/site_test_jenkins_test
+timeout 20s curl -NIs http://test.jenkins.columbiadevops.com/ | head -n 1 >> site_test_jenkins_test
 #cd /home/jenkins/dashboard/workspace
 #rm /home/jenkins/dashboard/workspace/sites-monitor.html
 #cp /home/jenkins/dashboard/workspace/sites-monitor.html.tmpl /home/jenkins/dashboard/workspace/sites-monitor.html
@@ -28,28 +28,28 @@ fi
 #X1 Jenkins
 if [[ `grep "200 OK" site_X1_jenkins_test` ]]
 then
-sed -i "s/2_yet_to_update/X1_UP/" /home/jenkins/dashboard/workspace/sites-monitor.html
-else
-sed -i "s/2_yet_to_update/X1_DOWN/" /home/jenkins/dashboard/workspace/sites-monitor.html
+#sed -i "s/2_yet_to_update/X1_UP/" /home/jenkins/dashboard/workspace/sites-monitor.html
+#else
+#sed -i "s/2_yet_to_update/X1_DOWN/" /home/jenkins/dashboard/workspace/sites-monitor.html
 #email here
 echo "X1 Jenkins was DOWN" | mailx -s "This Email Will Trigger Automatically" -a /var/www/html/sites-monitor.html pavan.sai@columbia.com
 fi
 #Test Jenkins
 if [[ `grep "200 OK" site_test_jenkins_test` ]]
 then
-sed -i "s/3_yet_to_update/Test_UP/" /home/jenkins/dashboard/workspace/sites-monitor.html
-else
-sed -i "s/3_yet_to_update/Test_DOWN/" /home/jenkins/dashboard/workspace/sites-monitor.html
+#sed -i "s/3_yet_to_update/Test_UP/" /home/jenkins/dashboard/workspace/sites-monitor.html
+#else
+#sed -i "s/3_yet_to_update/Test_DOWN/" /home/jenkins/dashboard/workspace/sites-monitor.html
 #email here
 echo "TEST Jenkins was DOWN" | mailx -s "This Email Will Trigger Automatically" -a /var/www/html/sites-monitor.html pavan.sai@columbia.com
 echo ": "---------------------"
 fi
 #Feed for Build Jenkins
-for i in `cut -d':' -f1 /home/jenkins/dashboard/workspace/site_monitor_feed`
-do
-inplace="${i}"
-replace=$(grep "$i" /home/jenkins/dashboard/workspace/site_monitor_feed|cut -d':' -f2)
-sed -i "s|`echo $inplace`|`echo $replace`|" /home/jenkins/dashboard/workspace/sites-monitor.html
-done
-cp /home/jenkins/dashboard/workspace/sites-monitor.html /var/www/html/
-chmod 777 /var/www/html/sites-monitor.html
+#3for i in `cut -d':' -f1 /home/jenkins/dashboard/workspace/site_monitor_feed`
+#do
+#inplace="${i}"
+#replace=$(grep "$i" /home/jenkins/dashboard/workspace/site_monitor_feed|cut -d':' -f2)
+#sed -i "s|`echo $inplace`|`echo $replace`|" /home/jenkins/dashboard/workspace/sites-monitor.html
+#done
+#cp /home/jenkins/dashboard/workspace/sites-monitor.html /var/www/html/
+#chmod 777 /var/www/html/sites-monitor.html
