4a82dbb6c67264f42f39b719c63087e371eb3355
saipavwan <40223002+saipavwan@users.noreply.github.com>
2019-03-01 11:12:46 -0800

    Update stack.sh

--- stack.sh
+++ stack.sh
@@ -5,28 +5,25 @@ umask 0002
 timeout 20s curl -Is http://build.jenkins.columbiadevops.com/ | head -n 1 > /home/jenkins/dashboard/workspace/site_Build_jenkins_test
 
 timeout 20s curl -Is http://jenkins.colmdev.com/ | head -n 1 > /home/jenkins/dashboard/workspace/site_X1_jenkins_test
-echo "                            *    *
-                        * *  * *
-                       * * * * * *
-                      * * * * * * *"
+
 timeout 20s curl -Is http://test.jenkins.columbiadevops.com/ | head -n 1 > /home/jenkins/dashboard/workspace/site_test_jenkins_test
 #cd /home/jenkins/dashboard/workspace
 #rm /home/jenkins/dashboard/workspace/sites-monitor.html
 #cp /home/jenkins/dashboard/workspace/sites-monitor.html.tmpl /home/jenkins/dashboard/workspace/sites-monitor.html
-now_date=`date +%m/%d-%H:%M:%S`
-later_date=`date --date='5 minutes' +%m/%d-%H:%M:%S`
+#now_date=`date +%m/%d-%H:%M:%S`
+#later_date=`date --date='5 minutes' +%m/%d-%H:%M:%S`
 #sed -i "s|Status Updated on:|Status Updated on: `echo $now_date`|" /home/jenkins/dashboard/workspace/sites-monitor.html
 #sed -i "s|Next Update at:|Next Update at: `echo $later_date`|" /home/jenkins/dashboard/workspace/sites-monitor.html
 #Build Jenkins
 if [[ `grep "200 OK" site_Build_jenkins_test` ]]
 then
-echo "checked the value for build jenkins"
+#echo "checked the value for build jenkins"
 #sed -i "s/1_yet_to_update/Build_UP/" /home/jenkins/dashboard/workspace/sites-monitor.html
-else
-echo "checked the value for X1 jenkkins"
-sed -i "s/1_yet_to_update/Builld_DOWN/" /home/jenkins/dashboard/workspace/sites-monitor.html
+#else
+#echo "checked the value for X1 jenkkins"
+#sed -i "s/1_yet_to_update/Builld_DOWN/" /home/jenkins/dashboard/workspace/sites-monitor.html
 #email here
-echo "Build Jenkins was DOWN" | mailx -s "This Email will Trigger Automatically" -a /var/www/html/sites-monitor.html pavan.sai@columbia.com
+echo "Build Jenkins was DOWN" | mailx -s "This Email will Trigger Automatically" -a pavan.sai@columbia.com
 fi
 #X1 Jenkins
 if [[ `grep "200 OK" site_X1_jenkins_test` ]]
